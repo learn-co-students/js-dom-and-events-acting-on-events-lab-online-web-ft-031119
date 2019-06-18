d96199c003ba3058547b22fecaa9d82e74a188fc
Daniel Schneider <daschne8@gmail.com>
2019-06-18 16:50:09 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,28 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+function retrieveEmployeeInformation(){
+  return document.body.querySelector('input').value
+}
+function addNewElementAsLi(){
+  const name = retrieveEmployeeInformation()
+  let targetUl = document.querySelector('.employee-list')
+  let element = document.createElement('li')
+  element.innerHTML = name
+  targetUl.appendChild(element)
+}
+function addNewLiOnClick(){
+  let submit = document.querySelector('input[type="submit"]')
+  let input = document.querySelector('input[type="text"]')
+  submit.addEventListener('click', function(){
+    addNewElementAsLi()
+    input.value = ''
+   })
+}
+function clearEmployeeListOnLinkClick(){
+  let clearLink = document.querySelector('a')
+  let employeeList = document.querySelector('ul.employee-list')
+  clearLink.addEventListener('click',function(){
+    employeeList.innerHTML = ''
+  })
+}

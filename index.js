7eff1535d487700b4abf5355e165105728202b0a
Jon <github email address>
2019-06-19 16:31:22 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,29 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+	return document.querySelector('input').value
+};
+
+function addNewElementAsLi(){
+  let employeeName = retrieveEmployeeInformation()
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employeeName}</li>`)
+};
+
+function addNewLiOnClick(){
+	document.querySelector('input[type="submit"]').addEventListener('click', function(event){
+    addNewElementAsLi()
+    clearInputValue()
+  })
+}
+
+function clearInputValue(){
+	document.querySelector('input').value = ""
+}
+
+function clearEmployeeListOnLinkClick(){
+	document.querySelector('a').addEventListener('click', function(event){
+		document.querySelector('ul').innerHTML = ''
+	})
+}

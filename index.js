e46abb2605b99b2bfbe5331508ca41e7f0682f95
filmscore88 <cmtmorales24@gmail.com>
2019-06-22 11:44:42 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+
+}
+
+function addNewElementAsLi(){
+  let li= document.createElement('li');
+  li.innerHTML= retrieveEmployeeInformation();
+  let ul= document.querySelector('ul');
+  ul.appendChild(li);
+}
+
+function addNewLiOnClick(){
+  let input= document.querySelector('input[value="Add Employee"]');
+  input.addEventListener('click', function(event){
+    addNewElementAsLi()
+    document.querySelector('input').value= "";
+  });
+}
+
+function clearEmployeeListOnLinkClick(){
+  let button= document.querySelector('a');
+  let ul= document.querySelector('ul');
+
+  button.addEventListener('click', function(e){
+    while(ul.firstChild){
+      ul.removeChild(ul.firstChild);
+    }
+  });
+}
